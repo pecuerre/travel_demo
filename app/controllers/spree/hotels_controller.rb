71661f3e08a852d591639686ad3486157c6af153
Maikel Arcia <macarci@gmail.com>
2015-10-05 14:27:17 -0400

    Generating sample list of hotels

--- app/controllers/spree/hotels_controller.rb
+++ app/controllers/spree/hotels_controller.rb
@@ -2,34 +2,70 @@ module Spree
   class HotelsController < Spree::StoreController
     helper 'spree/products'
     respond_to :html
-    
-    before_action :get_holels, only: [:list, :grid, :block]
+
+    before_action :get_hotels, only: [:list, :grid, :block]
 
     def index
+      list
     end
-    
+
     def list
     end
 
     def grid
     end
-    
+
     def block
     end
-    
+
     def detail
     end
-    
+
     def booking
     end
-    
+
     def thanks_you
     end
-    
-    private 
-      def get_holels
-        @products = Spree::Product.all
+
+    private
+    def get_hotels
+      @products = Spree::Product.sample
+    end
+
+  end
+
+  class Product
+
+    class << self
+
+      def generateProduct
+
+        product = OpenStruct.new
+        product.name = Faker::Company.name
+        product.description=Faker::DizzleIpsum.paragraph(5)
+        product.destination = Faker::DizzleIpsum.phrase
+        product.destination_taxon = Faker::DizzleIpsum.phrase
+        product.variant_images = []
+        product.images=[]
+        product.price = rand(100...1000)
+        product.review=rand(1000)
+        product
       end
 
+      def sample
+        @products = @@_products ||=
+            begin
+              products= []
+              sample_size=30
+              i=0
+              while i<sample_size
+                products << generateProduct
+                i+=1
+              end
+              products
+            end
+      end
+
+    end
   end
 end

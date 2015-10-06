338a238a21e89fa3b4e25334749f674e2f41eb86
Pedro Quintero <pecuerre@gmail.com>
2015-10-06 19:58:43 -0400

    filter do not show properties with zero products

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,4 +3,8 @@ Spree::Product.class_eval do
 	def self.with_property(property)
 		joins(:product_properties => :property).where('spree_properties.id' => property.id)
 	end
+
+	def self.with_property_type(property_type)
+		joins(:product_properties => {:property => :property_type}).where('spree_property_types.id' => property_type.id)
+	end
 end

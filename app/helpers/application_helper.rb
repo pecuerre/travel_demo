75663aaeead714994228d31b355cf8ef247dcf2e
Pedro Quintero <pecuerre@gmail.com>
2015-10-06 17:16:19 -0400

    improving filters url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,31 +4,35 @@ module ApplicationHelper
 		entity.name.downcase.sub(' ', '-')
 	end
 
+	def ptid(pt)
+		"pt#{pt.id}"
+	end
+
 	def is_property_in_url?(pp)
-		pt = "pt#{pp.property_type.id}"
+		pt = ptid(pp.property_type)
 		list = params[pt]
 		list = list.split('_') rescue []
 		list.include?(pp.id.to_s)
 	end
 
 	def add_property_to_url(pp)
-		pt = "pt#{pp.property_type.id}"
+		pt = ptid(pp.property_type)
 		list = params[pt]
 		list = "" unless list
 		list = list.split('_')
 		list << pp.id.to_s
 		list = list.uniq
 		str = list.join('_')
-		params.merge(pt => str)
+		params.merge(pt => str, :lastpt => pt)
 	end
 
 	def remove_property_from_url(pp)
-		pt = "pt#{pp.property_type.id}"
+		pt = ptid(pp.property_type)
 		list = params[pt]
 		list = "" unless list
 		list = list.split('_')
 		list.delete(pp.id.to_s)
 		str = list.join('_')
-		params.merge(pt => str)
+		params.merge(pt => str, :lastpt => pt)
 	end
 end

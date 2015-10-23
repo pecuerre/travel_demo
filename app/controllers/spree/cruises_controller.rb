096c3c5eaeb59f39d1372ea4490e51ada5aaa6c5
avaldesa <anelivaldes@gmail.com>
2015-10-23 10:10:59 -0400

    Loading sample cruises's data

--- app/controllers/spree/cruises_controller.rb
+++ app/controllers/spree/cruises_controller.rb
@@ -1,14 +1,18 @@
+require 'travel_demo/cruise'
+require 'travel_demo/booking_order'
+
 module Spree
   class CruisesController < Spree::StoreController
     helper 'spree/products'
     respond_to :html
+    before_action :get_cruises, only: [:list, :grid, :block, :detail, :booking, :thanks_you]
 
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
-    
+
     def list
     end
 
@@ -23,8 +27,14 @@ module Spree
 
     def booking
     end
-    
+
     def thanks_you
+    end
+
+    private
+    def get_cruises
+      @cruises= ::Sample::Cruise.sample
+      @booking=::Sample::Booking_Order.sample
     end
 
   end

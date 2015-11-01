87df1b3f310c8eec6d4434f74417568ecc9e39a5
Miguel Sancho <sanchojaf@gmail.com>
2015-11-01 18:09:53 -0500

    comment spree_i18n initializer

--- config/initializers/spree_i18n.rb
+++ config/initializers/spree_i18n.rb
@@ -1,2 +1,2 @@
-SpreeI18n::Config.available_locales = [:en, :es] # displayed on translation forms
-SpreeI18n::Config.supported_locales = [:en, :es] 
+# SpreeI18n::Config.available_locales = [:en, :es] # displayed on translation forms
+# SpreeI18n::Config.supported_locales = [:en, :es] 

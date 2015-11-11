2638c3d303cbf1dcae1a0488a54cd4e3fc65532b
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2015-11-11 17:13:10 -0500

    Allow add taxons with equal name in different taxonomy.

--- db/common/trip_functions.rb
+++ db/common/trip_functions.rb
@@ -19,7 +19,7 @@ module TripFunctions
   def create_taxon(taxonomy, taxon_attrs)
     taxon_attrs[:parent] = Spree::Taxon.where(:name => taxon_attrs[:parent]).first if taxon_attrs[:parent]
     taxon_attrs[:taxonomy] = taxonomy
-    taxon = Spree::Taxon.where(:name => taxon_attrs[:name]).first_or_create(taxon_attrs)
+    taxon = Spree::Taxon.where(:name => taxon_attrs[:name], :taxonomy => taxonomy).first_or_create(taxon_attrs)
     puts "Taxon: #{taxon_attrs[:name]}"
     taxon
   end

bb34f7201dac3598d8cf1b6c08c35ed0ac807d7f
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2015-11-26 00:20:12 -0500

    Update houses seacrh view

--- vendor/assets/javascripts/spree/frontend/scripts.js
+++ vendor/assets/javascripts/spree/frontend/scripts.js
@@ -45,42 +45,61 @@ tjq(document).ready(function () {
      * @param service {String} Remote service url. Ej: '/api/hotels'
      */
     tjq.fn.searchProducts = function (type, service, done) {
-        var data = tjq(this).serializeObject();
+        var data = tjq(this).serializeObject(),
+            date = tjq.datepicker.formatDate('yy-mm-dd', new Date()),
+            params = tjq(this).serialize(),
+            cache = JSON.parse(localStorage.getItem(type) || "false");
 
-        tjq.ajax({
-            url: service,
-            type: 'POST',
-            data: data,
+        // If not is in search-result-section go to it.
+        if (tjq('#search-result-items').length == 0) {
+            tjq('section#content div#main div.row').html('');
+            tjq('section#content div#main div.row').mustache('search-result-section');
+        }
 
-            success: function (response) {
-                // Save data in local storage.
-                localStorage.setItem(type, JSON.stringify({items: response}));
-                done(true);
-            },
+        if (!cache || cache.date != date || cache.service != service || cache.params != params) {
+            tjq.ajax({
+                url: service,
+                type: 'POST',
+                data: data,
 
-            error: function (response) {
-                var errors = JSON.parse(response.responseText);
+                success: function (response) {
+                    // Save data in local storage.
+                    localStorage.setItem(type, JSON.stringify({
+                        items: response,
+                        service: service,
+                        params: params,
+                        date: date
+                    }));
+                    done(true);
+                },
 
-                if (errors instanceof Array) {
-                    errors = errors.map(function (i) {
-                        return i.error
-                    })
-                } else {
-                    console.error(errors);
-                    errors = ['The failed request.', 'Please try again later.'];
+                error: function (response) {
+                    var errors = JSON.parse(response.responseText);
+
+                    if (errors instanceof Array) {
+                        errors = errors.map(function (i) {
+                            return i.error
+                        })
+                    } else {
+                        console.error(errors);
+                        errors = ['The failed request.', 'Please try again later.'];
+                    }
+
+                    tjq.msgBox({
+                        title: "Error",
+                        content: errors.join('<br/>'),
+                        type: "error",
+                        showButtons: true,
+                        opacity: 0.9
+                    });
+
+                    done(false, errors);
                 }
-
-                tjq.msgBox({
-                    title: "Error",
-                    content: errors.join('<br/>'),
-                    type: "error",
-                    showButtons: true,
-                    opacity: 0.9
-                });
-
-                done(false, errors);
-            }
-        });
+            });
+        } else {
+            tjq(this).unserializeForm(cache.params);
+            done(true);
+        }
     }
 
     /**
@@ -90,22 +109,47 @@ tjq(document).ready(function () {
      */
     tjq.fn.renderProducts = function (type) {
         var data = JSON.parse(localStorage.getItem(type) || '{"items":[]}'),
-            templateId = type + '-' + (localStorage.getItem('product-view-type') || 'list');
+            viewType = localStorage.getItem('products-view-type') || 'list';
 
         for (var i in mustacheHelpers) {
             data[i] = mustacheHelpers[i];
         }
 
+        // Show the amount of products found.
         tjq('.search-results-title b').html(data.items.length);
 
-        // Render response items.
-        tjq(this).html('');
-        tjq(this).mustache(templateId, data);
+        // Reconnect event to change products items view when clicked in list, grid or block options.
+        tjq('.products-view-type li a').off('click').on('click', function (e) {
+            var data = tjq(this).data();
 
-        // Create image gallery for each product.
+            localStorage.setItem('products-view-type', data.view);
+            tjq('#search-result-items').renderProducts(type);
+
+            e.preventDefault();
+        });
+
+        // Show response items.
+        tjq(this).html('');
+        tjq(this).mustache('products-view-as-' + viewType, data);
+
+        // Reconnect event to show product details when clicked in 'TITLE' or 'SELECT' button.
+        tjq('a.product-details').off('click').on('click', function (e) {
+            var data = tjq(this).data();
+
+            // TODO: Obtener data para la vista detalles.
+            console.log(data);
+
+            // Render response items.
+            tjq('section#content div#main div.row').html('');
+            tjq('section#content div#main div.row').mustache('product-details', data);
+
+            e.preventDefault();
+        });
+
         data.items.forEach(function (item) {
-            var id = '#product-gallery-' + item.id;
-            tjq(id).magnificPopup({
+
+            // Create image gallery for each product.
+            tjq('#product-gallery-' + item.id).magnificPopup({
                 type: 'image',
                 mainClass: 'mfp-img-mobile',
                 //showCloseBtn: false,
@@ -113,7 +157,7 @@ tjq(document).ready(function () {
                 gallery: {
                     enabled: true,
                     navigateByImgClick: true,
-                    preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+                    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
                 },
                 items: item.images.map(function (img) {
                     return {src: img}
@@ -135,6 +179,7 @@ tjq(document).ready(function () {
             url: service,
             type: method || 'GET',
             data: data || {},
+            async: false,
             contentType: 'application/json; charset=utf-8',
             success: function (response) {
                 response.forEach(function (item) {
